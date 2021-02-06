366589b4a5bce2e0cef7fa9871714de26ec1f937
fabien <fabientremblay@hotmail.com>
2021-02-06 10:18:51 -0500

    Exploration d'extraction PDF

--- extraireAO/pdfTexte/explorePdfMiner.py
+++ extraireAO/pdfTexte/explorePdfMiner.py
@@ -8,21 +8,25 @@ import os
 
 listeFichiers = os.scandir("Sources");
 
+nbAvec = 0;
+nbSans = 0;
 for fichier in listeFichiers :
-   print(fichier);
-
-fp = open('Sources/AO_CIUSSS_CEMTL-2020-047-Charge-projets-TI.pdf', 'rb')
-# Create a PDF parser object associated with the file object.
-parser = PDFParser(fp)
-document = PDFDocument(parser)
-
-if not document.is_extractable:
-    raise PDFTextExtractionNotAllowed
-
-try :
-    outlines = document.get_outlines();
-except PDFNoOutlines :
-    		
-
-for (level,title,dest,a,se) in outlines:
-    print (level, title)
+    print(fichier);
+    if fichier.is_file() :
+        fp = open('Sources/{0}'.format(fichier.name), 'rb')
+        # Create a PDF parser object associated with the file object.
+        parser = PDFParser(fp)
+        document = PDFDocument(parser)
+        
+        if not document.is_extractable:
+            raise PDFTextExtractionNotAllowed
+        
+        try :
+            outlines = document.get_outlines();
+            nbAvec += 1;
+            for (level,title,dest,a,se) in outlines:
+                print (level, title)
+        except PDFNoOutlines :
+            nbSans += 1;
+            print("Fichier : {0}\n  Table de matières non trouvée".format(fichier));
+    
