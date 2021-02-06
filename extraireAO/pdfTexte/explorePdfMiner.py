1ea89951779c92900a71973f6379b8fad6290bd3
fabien <fabientremblay@hotmail.com>
2021-02-06 10:09:06 -0500

    Exploration d'extraction PDF

--- extraireAO/pdfTexte/explorePdfMiner.py
+++ extraireAO/pdfTexte/explorePdfMiner.py
@@ -2,14 +2,27 @@ from pdfminer.pdfparser import PDFParser
 from pdfminer.pdfdocument import PDFDocument
 from pdfminer.pdfpage import PDFTextExtractionNotAllowed
 
-fp = open('mypdf.pdf', 'rb')
+# -- Outils pour le scan du répertoire
+import glob
+import os
+
+listeFichiers = os.scandir("Sources");
+
+for fichier in listeFichiers :
+   print(fichier);
+
+fp = open('Sources/AO_CIUSSS_CEMTL-2020-047-Charge-projets-TI.pdf', 'rb')
 # Create a PDF parser object associated with the file object.
 parser = PDFParser(fp)
 document = PDFDocument(parser)
 
 if not document.is_extractable:
     raise PDFTextExtractionNotAllowed
-    
-outlines = document.get_outlines()
+
+try :
+    outlines = document.get_outlines();
+except PDFNoOutlines :
+    		
+
 for (level,title,dest,a,se) in outlines:
     print (level, title)
